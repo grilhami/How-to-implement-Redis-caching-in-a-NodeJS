b49ff6a6d9c9f6da1ab2045f8bee54aae4d05161
grilhami <gilangrilhami@hotmail.co.uk>
2020-07-29 23:40:06 +0700

    Add index.json and populate file

---
+++ code/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send(
+    `Let's implement Redis caching in NodeJS!`
+  );
+});
+
+app.listen(PORT, () => {
+  console.log("Listening in port " + PORT);
+});
