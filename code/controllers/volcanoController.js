e92925934482b63f597900c8941fd61b208c728e
grilhami <gilangrilhami@hotmail.co.uk>
2020-08-04 00:36:00 +0700

    Add route and controller for getting all volcanos

--- code/controllers/volcanoController.js
+++ code/controllers/volcanoController.js
@@ -2,6 +2,13 @@ const { volcano } = require('../models');
 
 
 module.exports = {
+    getVolcanos: (req, res) => {
+        volcano.findAll().then(result => 
+            res.status(200).send({message: "Get Volcanos", result})
+        ).catch(err => 
+            res.status(500).send({message: "Server Error", error: err.message})
+        )
+    },
     createVolcano: (req, res) => {
         const { name, shape, elevation, lastEruption} = req.body;
 
@@ -13,6 +20,6 @@ module.exports = {
                 lastEruption
             },
         ).then(result => res.status(200).send({message: "Volcano Created", result})
-        ).catch(err => res.status(200).send({message: "Server Error", error: err.message}))
+        ).catch(err => res.status(500).send({message: "Server Error", error: err.message}))
     }
 };
--- code/routes/volcanoRoute.js
+++ code/routes/volcanoRoute.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { volcanoC } = require('../controllers');
 const router = Router();
 
+router.get("", volcanoC.createVolcano);
 router.post("", volcanoC.createVolcano);
 
 module.exports = router;
