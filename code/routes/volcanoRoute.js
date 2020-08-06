f469a9760875e78ad15d40c31a75cf43d85f515d
grilhami <gilangrilhami@hotmail.co.uk>
2020-08-06 23:28:06 +0700

    Add route for getting all volcanos

--- code/routes/volcanoRoute.js
+++ code/routes/volcanoRoute.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { volcanoC } = require('../controllers');
 const router = Router();
 
-router.get("", volcanoC.createVolcano);
+router.get("", volcanoC.getVolcanos);
 router.post("", volcanoC.createVolcano);
 
 module.exports = router;
